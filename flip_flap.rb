9683e3aff1aa8b17e796d4f15373eb0d06a2f949
Katy Wu <katy12032001@gmail.com>
2015-10-10 22:51:23 +0800

    peer grading

--- flip_flap.rb
+++ flip_flap.rb
@@ -1,15 +1,17 @@
 require_relative 'tsv_buddy'
+require_relative 'yaml_buddy'
 require 'yaml'
 
 # a class that is able to transfer between YSML
 class FlipFlap
   include TsvBuddy
   attr_accessor :data
-  def take_yaml(yml)
-    @data = YAML.load(yml)
-  end
-
-  def to_yaml
-    @data.to_yaml
-  end
+  include YamlBuddy
+  # def take_yaml(yml)
+  #   @data = YAML.load(yml)
+  # end
+  #
+  # def to_yaml
+  #   @data.to_yaml
+  # end
 end
